fe2ec76808470906f2bfea46bdfd5a1728fda71d
mark <sund@haidilao.com>
2019-09-25 18:02:20 +0800

    test demo

---
+++ src/components/child.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="box">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  width: 750px;
+  height: 100%;
+}
+</style>
+
+<script>
+export default {
+  created() {
+    this.ddd()
+  },
+  methods: {
+    ddd() {
+      console.log('dasdasdsad')
+    }
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <child @hook:ddd="dd"></child>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import child from "@/components/child.vue";
+
+export default {
+  name: "home",
+  components: {
+    child
+  },
+  methods: {
+    dd() {
+      console.log("dasdadasd");
+    }
+  }
+};
+</script>
